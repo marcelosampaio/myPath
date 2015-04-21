cfb152c1960cb7440853c17ddfa1e0b765c4bfa7
Marcelo Sampaio <marcelo@artlogica.com.br>
2015-04-21 20:34:00 -0300

    Initial Commit

---
+++ MyPath/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyPath
+//
+//  Created by Marcelo Sampaio on 4/21/15.
+//  Copyright (c) 2015 Marcelo Sampaio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyPath/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyPath
+//
+//  Created by Marcelo Sampaio on 4/21/15.
+//  Copyright (c) 2015 Marcelo Sampaio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
