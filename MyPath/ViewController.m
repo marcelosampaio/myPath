27ca7fe0c1abd10a906da5afc607f1f16d734e87
Marcelo Sampaio <marcelo@artlogica.com.br>
2015-04-21 21:35:58 -0300

    checkpoint commit

--- MyPath/ViewController.m
+++ MyPath/ViewController.m
@@ -8,6 +8,9 @@
 
 #import "ViewController.h"
 
+#define DEFAULT_STRING_START_ACTION             @"Start"
+#define DEFAULT_STRING_STOP_ACTION             @"Stop"
+
 @interface ViewController ()
 
 @end
@@ -23,7 +26,10 @@
     
     // UI
     self.isOn=NO;
-    [self.startStopButton setTitle: @"Start Collecting Locations" forState: UIControlStateNormal];
+    [self.startStopButton setTitle:DEFAULT_STRING_START_ACTION forState: UIControlStateNormal];
+    self.startStopButton.layer.cornerRadius=50;
+    self.startStopButton.layer.masksToBounds=YES;
+    self.startStopButton.backgroundColor=[UIColor colorWithRed:0.2 green:0.75f blue:0.1 alpha:2];
     
     
     
@@ -32,7 +38,15 @@
 
 #pragma mark - UI Actions
 - (IBAction)startStopAction:(id)sender {
-
+    if (self.isOn) {
+        [self.startStopButton setTitle:DEFAULT_STRING_START_ACTION forState: UIControlStateNormal];
+        self.startStopButton.backgroundColor=[UIColor colorWithRed:0.2 green:0.75f blue:0.1 alpha:2];
+        self.isOn=NO;
+    }else{
+        [self.startStopButton setTitle:DEFAULT_STRING_STOP_ACTION forState: UIControlStateNormal];
+        self.startStopButton.backgroundColor=[UIColor redColor];
+        self.isOn=YES;
+    }
 }
 
 
