f786ac20fe7495cadd664284d465426f8c0c98b7
Marcelo Sampaio <marcelo@artlogica.com.br>
2015-05-10 21:57:14 -0300

    checkpoint commit II

--- MyPath/AppConfig.h
+++ MyPath/AppConfig.h
@@ -9,22 +9,24 @@
 #ifndef MyPath_AppConfig_h
 #define MyPath_AppConfig_h
 
-
-// -----------------------------
 #pragma mark - String Constants
 
-// String
 #define DEFAULT_STRING_START_ACTION             @"S T A R T"
 #define DEFAULT_STRING_STOP_ACTION              @"S T O P"
 #define DEFAULT_STRING_SHOW_LOCATIONS_ACTION    @"Locations"
 
+
+
 #pragma mark - MapKit Constants
-// Map Kit
+
 #define DEFAULT_MAPKIT_MAXSPAN                  0.025f
 #define DEFAULT_MAPKIT_DISTANCE_FROM_CENTER     800.0
 
+
+
+
 #pragma mark - Color Constants
-// Color
+
 #define DEFAULT_COLOR_GREEN                     [UIColor colorWithRed:0.1f green:0.75f blue:0.1f alpha:1.00f];
 #define DEFAULT_COLOR_GREEN_ALPHA               [UIColor colorWithRed:0.1f green:0.75f blue:0.1f alpha:0.25f];
 #define DEFAULT_COLOR_RED                       [UIColor colorWithRed:1.0f green:0.15f blue:0.15f alpha:1.00f];
@@ -32,10 +34,10 @@
 #define DEFAULT_COLOR_LOCATIONS_OUTLET          [UIColor colorWithRed:0.1f green:0.3f blue:0.05f alpha:1.00f];
 
 
-// --------------------------
+
+
 #pragma mark - Database Constants
 
-// Database
 #define DATABASE_IDENTIFIER                     @"MyPath.db"
 
 
